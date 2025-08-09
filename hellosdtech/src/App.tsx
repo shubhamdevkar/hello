f0f07a7d0932774bac162ad7f6044528523060e7
shubham devkar <shubhamdevkar@users.noreply.github.com>
2025-08-09 13:45:49 +0530

    'hi'

---
+++ hellosdtech/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='bg-white flex justify-center '>
+      <h1 className='text-3xl font-bold text-blue-500'>Hello World</h1>
+    </div>
+
+  )
+}
+
+export default App
