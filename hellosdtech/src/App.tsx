d16ccee37964c763ae258829ad778c57b70e245a
shubham devkar <shubhamdevkar@users.noreply.github.com>
2025-08-09 14:07:20 +0530

    'logo'

--- hellosdtech/src/App.tsx
+++ hellosdtech/src/App.tsx
@@ -1,7 +1,8 @@
+import logo from './assets/logo1.svg';
 const App = () => {
   return (
     <div className='bg-white flex justify-center '>
-      <h1 className='text-3xl font-bold text-blue-500'>Hello World</h1>
+      <img src={logo} alt="sdtech logo" /><h1 className='text-3xl font-bold text-blue-500'>Hello World</h1>
     </div>
 
   )
